cad6a75145b153e485d9971107b6b08cfa68f2a6
masum <masumpatel39@gmail.com>
2020-07-16 18:21:38 +0530

    My first Flask app in Heroku

--- app/views.py
+++ app/views.py
@@ -6,10 +6,11 @@ import tensorflow as tf
 from tensorflow import keras
 import os
 
-UPLOAD_FOLDER = "app/uploads"
+UPLOAD_FOLDER = "uploads/"
 
 NEURAL_NET_MODEL_PATH = "app/model/model_weights.h5"
 NEURAL_NET = tf.keras.models.load_model(NEURAL_NET_MODEL_PATH)
+graph = tf.get_default_graph()
 
 brands = {0:'Honda', 1:'Hyundai', 2:'Lexus', 3:'Toyota', 4:'Volkswagon'}
 img_x=img_y=70
@@ -20,19 +21,21 @@ def upload_image():
     if request.method == "POST":
         if request.files:
             image_file = request.files["image"]
+            print(image_file)
             if image_file:
                 passed = False
-            try:
                 filename = image_file.filename
                 filepath = os.path.join(UPLOAD_FOLDER, filename)
                 image_file.save(filepath)
                 passed = True
-
-            except Exception:
+            
+            else:
                 passed = False
                 print(passed)
             if passed:
                 return redirect(url_for('predict', filename=filename))
+        else:
+            print("no file")
     return render_template("upload_image.html")
 
 def load_and_prepare(filepath):
@@ -45,14 +48,16 @@ def load_and_prepare(filepath):
 @app.route('/predict/<filename>')
 def predict(filename):
     
-    prediction = ''
-    image_url = url_for('images', filename=filename)
-    image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    image_data = load_and_prepare(image_path)
-    preds = NEURAL_NET.predict(image_data)
-    for i in range(len(preds[0])):
-        if np.around(preds[0][i]) == 1:
-            prediction = brands[i]
+    global graph
+    with graph.as_default():
+        prediction = ''
+        image_url = url_for('images', filename=filename)
+        image_path = os.path.join(UPLOAD_FOLDER, filename)
+        image_data = load_and_prepare(image_path)
+        preds = NEURAL_NET.predict(image_data)
+        for i in range(len(preds[0])):
+            if np.around(preds[0][i]) == 1:
+                prediction = brands[i]
 
     return render_template(
         'predict.html',
